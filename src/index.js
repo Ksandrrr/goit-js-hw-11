4fba916361f65d4d7f2e231ffafc3f1042d19f46
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-01-24 16:40:36 +0200

    #5

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 let page = 1;
 let currentSearch;
 const API_KEY = `33025526-c7f8a1e0e4b08b9a5d2f6635c`;
 
-
 const formSearch = document.querySelector(`#search-form`);
-const searchInput = document.querySelector(`#codeInput`);  
+const searchInput = document.querySelector(`#codeInput`);
 const listCard = document.querySelector(`.list-card`);
-const arrow = document.querySelector(`.fixed_btn`)
+const arrow = document.querySelector(`.fixed_btn`);
 arrow.style.opacity = `0`;
 
-const fetchPixabayAPI = (searchTerm,page) => {
+const fetchPixabayAPI = (searchTerm, page) => {
   return new Promise((resolve, reject) => {
-   axios.get("https://pixabay.com/api/", {
-      params: {
-        key: API_KEY,
-        q: searchTerm,
-        image_type: "photo",
-        orientation: "horizontal",
-       safesearch: true,
-       per_page: 40,  
-       page: page
-      }
-    }).then(response => {
-      resolve(response.data);
-      reject(new Error("Failed to retrieve images"));
-    })
+    axios
+      .get('https://pixabay.com/api/', {
+        params: {
+          key: API_KEY,
+          q: searchTerm,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+          page: page,
+        },
+      })
+      .then(response => {
+        resolve(response.data);
+        reject(new Error('Failed to retrieve images'));
+      });
   });
-}
+};
 
 let valueInput;
 let cardLength;
@@ -39,43 +40,40 @@ let isSubmitted = false;
 formSearch.addEventListener(`submit`, searchPixabay);
 
 function searchPixabay(e) {
-  e.preventDefault()
+  e.preventDefault();
   clearCardInterface()
-  page = 1
+  page = 1;
   const {
-    elements: { searchQuery }
+    elements: { searchQuery },
   } = e.currentTarget;
   currentSearch = searchQuery.value;
 
-  valueInput = searchQuery.value
+  valueInput = searchQuery.value;
   fetchPixabayAPI(valueInput)
- 
-   .then(images => {
-    if (searchQuery.value.length === 0) {
-      clearCardInterface()
-     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-    } else if (images.totalHits === 0) {
-      clearCardInterface()
-    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-    }else if (searchQuery.value.length > 0 && images.totalHits > 0) {
-      creatCardList(images)
-    }
-    if (isSubmitted && images.totalHits > 0) {
-      Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`)
-    }
-    isSubmitted = true;
-   })
-  .catch(err => Notiflix.Notify.failure(err))
-
-}
-
-
-function clearCardInterface() {
-  listCard.innerHTML = ``;
+    .then(images => {
+      if (searchQuery.value.length === 0) {
+        clearCardInterface();
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else if (images.totalHits === 0) {
+        clearCardInterface();
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else if (searchQuery.value.length > 0 && images.totalHits > 0) {
+        creatCardList(images);
+      }
+      if (isSubmitted && images.totalHits > 0) {
+        Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+      }
+      isSubmitted = true;
+    })
+    .catch(err => Notiflix.Notify.failure(err));
 }
 
 function creatCardList(array) {
-  const markupArray = array.hits.map((image) => {
+  const markupArray = array.hits.map(image => {
     return `
     <div class="photo-card">
         <a class="link-large_Photo" href="${image.largeImageURL}" onclick="return false"><img class="photo_card_small" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="200" height="152"/>
@@ -103,59 +101,57 @@ function creatCardList(array) {
   });
   listCard.innerHTML += markupArray.join('');
   const lightbox = new SimpleLightbox('.photo-card a');
-  lightbox.on('show.simplelightbox', function () {
-	showCounter = false
-});
- cardLength = document.querySelectorAll(`.photo-card`)
-    
+
+  cardLength = document.querySelectorAll(`.photo-card`);
 }
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
     page += 1;
-    fetchPixabayAPI(currentSearch, page)
-      .then(images => {
-        creatCardList(images);
-         arrow.style.opacity = `1`;
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight && cardLength.length === images.totalHits) {
-          Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
-        }
-        
-      })
+    fetchPixabayAPI(currentSearch, page).then(images => {
+      creatCardList(images);
+      arrow.style.opacity = `1`;
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+        cardLength.length === images.totalHits
+      ) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    });
   }
-      
 });
 
 function clearCardInterface() {
-listCard.innerHTML = ``;
+  listCard.innerHTML = ``;
 }
 
 ////// *** //////
 
-
-const code = ["Search images..."];
+const code = ['Search images...'];
 let index = 0;
 let letterIndex = 0;
 
-let currentText = "";
+let currentText = '';
 function updateCode() {
-    currentText = code[index].substring(0, letterIndex);
-    searchInput.setAttribute("placeholder", currentText);
-    if (letterIndex === code[index].length) {
-        if (index === code.length - 1) {
-            index = 0;
-        } else {
-            index++;
-        }
-        letterIndex = 0;
-        setTimeout(updateCode, 10000)
+  currentText = code[index].substring(0, letterIndex);
+  searchInput.setAttribute('placeholder', currentText);
+  if (letterIndex === code[index].length) {
+    if (index === code.length - 1) {
+      index = 0;
     } else {
-        letterIndex++;
-        setTimeout(updateCode, 80);
+      index++;
     }
+    letterIndex = 0;
+    setTimeout(updateCode, 10000);
+  } else {
+    letterIndex++;
+    setTimeout(updateCode, 80);
+  }
 }
 updateCode();
 arrow.addEventListener(`click`, () => {
   arrow.style.opacity = `0`;
-  searchInput.value = ``
-})
+  searchInput.value = ``;
+});
